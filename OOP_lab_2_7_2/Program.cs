073d774951a3c5d01749ac24e1ba9872bc8b7ff4
Роман Данилевич <dwordchanel@gmail.com>
2020-04-22 20:09:24 +0300

    Add project files.

---
+++ OOP_lab_2_7_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OOP_lab_2_7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (double x = -1 * Math.PI / 2; x < 3 * Math.PI / 2; x += Math.PI / 3)
+            {
+                double y = Math.Cos(x) * Math.Cos(x) + 3 * Math.Sin(x);
+                Console.WriteLine("x = {0:f4} y = {1:f4}", x, y);
+            }
+        }
+    }
+}
